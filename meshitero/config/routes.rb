983cdf280a5babb2c0788b1eb54a719980afe66d
fono <fono@pixiv.co.jp>
2022-06-23 20:44:50 +0900

    Bootstrap導入以外全部

--- meshitero/config/routes.rb
+++ meshitero/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create]
+    resource :favorites, only: [:create, :destroy]
+  end
 
   devise_for :users
   get 'homes/top'
